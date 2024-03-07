2cb461f19231abf13bb200cbf11282a809d64587
denisigorevich <dr.hy6yc@gmail.com>
2024-03-07 14:22:26 +0300

    Добавил шаблоны html страниц. Добавил requirements.txt

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Bot.db'
+db = SQLAlchemy(app)
+
+isDATABASE = False
+
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    login = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/create_db")
+def create_db():
+    # global isDATABASE
+    # if not isDATABASE:
+    #     app.app_context().push()
+    #     db.create_all()
+    #     isDATABASE = True
+    return render_template('create_db.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
