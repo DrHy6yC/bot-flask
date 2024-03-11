d4afc2173ae15595621bc7e81a207236953b4018
denisigorevich <dr.hy6yc@gmail.com>
2024-03-11 18:54:59 +0300

    Добавил шифрование паролей

--- app.py
+++ app.py
@@ -2,19 +2,20 @@ from icecream import ic
 
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Bot.db'
 db = SQLAlchemy(app)
 
-isDATABASE = False
+SECRET_KEY = 'edsgntu74nf$%@h3jUQ,slfkdccfde4232[q[e[dxldc.vlkekeokzkaiqkwm432kse'
 
 
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     login = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
 
 
 @app.route("/")
@@ -22,13 +23,12 @@ def index():
     return render_template('index.html', title="Главная")
 
 
-@app.route("/create_db")
+@app.route("/create_db", methods=['POST', 'GET'])
 def create_db():
-    # global isDATABASE
-    # if not isDATABASE:
-    #     app.app_context().push()
-    #     db.create_all()
-    #     isDATABASE = True
+    if request.method == 'POST':
+        ic(bool(request.get_data(as_text=True)))
+        if request.get_data(as_text=True):
+            db.create_all()
     return render_template('create_db.html')
 
 
@@ -36,11 +36,16 @@ def create_db():
 def auth():
     ic(request, type(request))
     if request.method == 'POST':
+        passwd = generate_password_hash(request.form['password'], 'pbkdf2')
         ic(request.form['login'])
         ic(request.form['password'])
+        ic(passwd)
+        user = User(login=request.form['login'], password=passwd)
+        db.session.add(user)
+        db.session.commit()
 
     return render_template('auth.html', title="Вход в приложение")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
